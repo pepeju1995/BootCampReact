871cc1568c804e70ce123f6ba302085d78798e22
pepeju1995 <pepeju95@gmail.com>
2021-09-11 11:17:06 +0200

    fixed button v2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,28 +5,29 @@ import { Find } from "./Components/FindForm";
 
 const App = () => {
   const [countries, setCountries] = useState([])
-  const [newSearch, setNewSearch] = useState('')
+  const [allCountries, setAllCountries] = useState([])
+  const [newFilter, setNewFilter] = useState('')
 
   useEffect(() => {
     axios.get('https://restcountries.eu/rest/v2/all')
       .then((response) => {
-        setCountries(response.data)
+        setAllCountries(response.data)
       })
   }, [])
 
   const handleChange = (event) => {
-    setNewSearch(event.target.value)
-    if(newSearch){
-      const regex = RegExp(newSearch, 'i')
-      const filterCountries = countries.filter(countrie => countrie.name.match(regex))
-      setCountries(filterCountries)
-    }
+    const search = event.target.value
+    
+    const regex = RegExp(search, 'i')
+    const filterCountries = allCountries.filter(countrie => countrie.name.match(regex))
+    setCountries(filterCountries)
+    setNewFilter(event.target.value)
   }
 
   return (
     <div>
       <h1>Countries</h1>
-      <Find handleChange={handleChange} value={newSearch}/>
+      <Find handleChange={handleChange} value={newFilter}/>
       <Countries countries={countries} setCountries={setCountries}/>
     </div>
   );
