6aa5b3b680cdc7068c2a5dd399447e34be0f5175
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-09-20 17:52:56 +0200

    Primeros pasos con express

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,7 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -21,11 +24,49 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-type': 'application/json'})
-    response.end(JSON.stringify(notes))
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if(note){
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+  }
+
+  notes = [...notes, newNote]
+
+  response.json(newNote)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is running in port ${PORT}`)
+})
