51aeec9d8c79da7e17bdebe6c79bd40133b9d36c
pepeju1995 <pepeju95@gmail.com>
2022-06-11 08:50:46 +0200

    # middleware

--- part3/index.js
+++ part3/index.js
@@ -1,27 +1,32 @@
 const express = require('express')
 const app = express()
+const logger = require('./loggerMiddleware')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
+app.use(logger)
+
 let notes = [
-    {
-      id: 1,
-      content: "HTML is easy and CSS to",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
+  {
+    id: 1,
+    content: 'HTML is easy and CSS to',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
 ]
 
 app.get('/', (request, response) => {
@@ -35,7 +40,7 @@ app.get('/api/notes', (request, response) => {
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
-  if(note){
+  if (note) {
     response.json(note)
   } else {
     response.status(404).end()
@@ -44,7 +49,7 @@ app.get('/api/notes/:id', (request, response) => {
 
 app.post('/api/notes', (request, response) => {
   const note = request.body
-  
+
   const ids = notes.map(note => note.id)
   const maxId = Math.max(...ids)
 
@@ -64,6 +69,12 @@ app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
   response.status(204).end()
+})
+
+app.use((request, response) => {
+  response.status(404).json({
+    error: 'Not found'
+  })
 })
 
 const PORT = 3001
